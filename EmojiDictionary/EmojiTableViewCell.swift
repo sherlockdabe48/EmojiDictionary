b289c1210cb58f856c4bedc6bbcb8fb252ac3d59
sherlockdabe48 <sherlockdabe48@gmail.com>
2021-05-17 23:40:42 +0700

    test commit

--- EmojiDictionary/EmojiTableViewCell.swift
+++ EmojiDictionary/EmojiTableViewCell.swift
@@ -17,7 +17,7 @@ class EmojiTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+     
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
